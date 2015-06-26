d85a68e36443bfcdeed2d8f1f3960d1596ef762a
kartik kapoor <kapoorkkartik@gmail.com>
2015-06-27 00:12:05 +0530

    Create catchtheball.py

---
+++ catchtheball.py
@@ -0,0 +1,48 @@
+import simplegui
+import random
+
+FRAME_WIDTH=STAGE_WIDTH=GROUND_WIDTH=821
+FRAME_HEIGHT=498
+STAGE_HEIGHT=FRAME_HEIGHT-30
+PADDLE_HEIGHT=STAGE_HEIGHT
+PADDLE_WIDTH=8
+PADDLE_POS=[STAGE_WIDTH/2,PADDLE_HEIGHT]
+
+image=simplegui.load_image("http://mrnussbaum.com/calendarclowns1/images/game_background.png")
+
+list_of_balls=[]
+
+colors=['Aqua','Blue','Fuchsia','Gray',
+        'Green','Lime','Maroon','Navy','Olive',
+        'Orange','Purple','Red','Silver','Teal',
+        'White','Yellow']
+
+
+class Ball:
+    def __init__(self,color,radius,x_location):
+        self.radius=radius
+        self.color=color
+        self.location=[x_location,0]
+        
+
+def timer():
+    radius = 10
+    color = random.choice(colors)
+    x_location = random.randint(20, STAGE_WIDTH-20)
+    new_ball = Ball(color,radius, x_location)
+    list_of_balls.append(new_ball)
+    
+        
+def draw(canvas):
+    canvas.draw_image(image,[FRAME_WIDTH/2,FRAME_HEIGHT/2],[FRAME_WIDTH,FRAME_HEIGHT],[FRAME_WIDTH/2,FRAME_HEIGHT/2],[FRAME_WIDTH,FRAME_HEIGHT])
+    
+    for ball in list_of_balls:
+        ball.location[1]+=5
+        canvas.draw_circle(ball.location,ball.radius,10,ball.color,ball.color)
+  
+    
+frame=simplegui.create_frame("ball",FRAME_WIDTH,FRAME_HEIGHT)
+timer=simplegui.create_timer(2000,timer)
+frame.set_draw_handler(draw)
+frame.start()
+timer.start()
